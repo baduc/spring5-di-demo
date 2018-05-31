2618978c33f86ebdf9437f99690a4417a20cdfac
Nguyen Ba Duc <baduc.nguyen1992@gmail.com>
2018-05-31 10:02:33 +0700

    manual dependency injection

---
+++ src/test/java/guru/springframework/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.GreetingServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PropertyInjectedControllerTest {
+
+    private PropertyInjectedController propertyInjectedController;
+
+    @Before
+    public void setUp() throws Exception {
+        propertyInjectedController = new PropertyInjectedController();
+        propertyInjectedController.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    public void sayHello() {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, propertyInjectedController.greetingService.sayGreeting());
+    }
+}
