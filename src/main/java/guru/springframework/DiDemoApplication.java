53bc45d78c36b4976ec17c0c8599e3923bc0c3e4
Nguyen Ba Duc <baduc.nguyen1992@gmail.com>
2018-06-07 16:50:00 +0700

    properties file

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -4,6 +4,7 @@ import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.MyController;
 import guru.springframework.controllers.PropertyInjectedController;
 import guru.springframework.controllers.SetterInjectedController;
+import guru.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +17,11 @@ public class DiDemoApplication {
 		MyController myController = (MyController) ctx.getBean("myController");
 
 		System.out.println(myController.hello());
+
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUsername());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getUrl());
 //		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 //		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
 //		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
